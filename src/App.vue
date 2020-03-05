5148e02cbd4e70f5cbdae64bbec328bf66b434dd
kim-99 <1424534564@qq.com>
2020-03-05 16:37:12 +0800

    vue框架应用

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <app-header></app-header>
+    <app-main></app-main>
+    <app-footer></app-footer>
+    
   </div>
 </template>
 
 <script>
+import appHeader from '@/components/AppHeader';
+import appFooter from '@/components/AppFooter';
+import appMain from '@/components/AppMain';
+
 export default {
-  name: 'App'
+  name: 'App',
+  data(){//储存数据
+    return {
+
+    };
+  },
+  methods: {//储存方法
+
+  },
+  components: {//注册组建
+    appHeader,//appHeader: appHeader,
+    appFooter,
+    appMain,
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='scss'>
+    // @import url(./assets/css/css.css);
+    @import './assets/css/css.css';
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,63 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '@/views/index'//@就是指src目录
+import NewsIndex from '@/views/news/index'
+import NewsList from '@/views/news/list'
+import NewsDetail from '@/views/news/detail'
+import ProductIndex from '@/views/product/index'
+import ProductList from '@/views/product/list'
+import ProductDetail from '@/views/product/detail'
+import Contact from '@/views/contact'
+
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'index',
+      component: Index
+    },
+    {
+      path:'/news',
+      name:'news',
+      component:NewsIndex,
+      children:[
+        {
+          path:'list',
+          name:'news list',
+          component:NewsList
+        },
+        {
+          path:'detail:id',
+          name:'news detail',
+          component:NewsDetail
+        }
+      ]
+    },
+    {
+      path:'/product',
+      name:'products',
+      component:ProductIndex,
+      children:[
+        {
+          path:'list',
+          name:'product list',
+          component:ProductList
+        },
+        {
+          path:'detail:id',
+          name:'product detail',
+          component:ProductDetail
+        }
+      ]
+    },
+    {//配置一个路由
+      path: '/contact',
+      name: 'contact',
+      component: Contact
+    },
   ]
 })
